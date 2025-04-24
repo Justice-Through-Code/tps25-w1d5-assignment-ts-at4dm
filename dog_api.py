6258ceb2261c77f9f4e6a135815bdc26506315df
ts-at4dm <terran.stone@maine.edu>
2025-04-23 23:00:17 -0400

    Completed Assignment

--- dog_api.py
+++ dog_api.py
@@ -47,20 +47,42 @@ def get_all_breeds():
 
 def get_random_image(breed):
     """GET request to fetch a random image from a breed."""
-    # TODO: Make a request to https://dog.ceo/api/breed/{breed}/images/random
-    # TODO: Return the image URL or handle errors
-    pass
+    url = f"https://dog.ceo/api/breed/{breed}/images/random"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        if data['status'] == 'success':
+            return data['message']
+        else:
+            return "Error: Unable to fetch image."
+    except requests.exceptions.RequestException:
+        return "Error: Could not connect to the API."
 
+        
+    
 def get_random_sub_breed_image(breed, sub_breed):
     """GET request to fetch a random image from a sub-breed."""
     # TODO: Make a request to https://dog.ceo/api/breed/{breed}/{sub_breed}/images/random
     # TODO: Return the image URL or handle errors
-    pass
+    url = f"https://dog.ceo/api/breed/{breed}/{sub_breed}/images/random"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        if data['status'] == 'success':
+            return data['message']
+        else:
+            return "Error: Unable to fetch sub-breed image."
+    except requests.exceptions.RequestException:
+        return "Error: Could not connect to the API."
 
 def show_breeds(breeds_dict):
     """Prints all available breeds 5 per line."""
     # TODO: Print all breeds (sorted), 5 per line
-    pass
+    breed_list = sorted(breeds_dict.keys())
+    for i in range(0, len(breed_list), 5):
+        print(", ".join(breed_list[i:i+5]))
 
 def main():
     while True:
